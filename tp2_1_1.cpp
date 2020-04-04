2964f45e903a347531562d46da688ca1580829ec
celeste <mesientofuerte22@gmail.com>
2020-04-04 16:30:24 -0300

    Punto2 apartado a

---
+++ tp2_1_1.cpp
@@ -0,0 +1,16 @@
+
+
+#define N 4
+#define M 5
+int f,c;
+double mt[N][M];
+
+for(f = 0;f<N; f++)
+{
+    for(f = 0;f<N; f++)
+    {
+        cprintf("%lf", mt[f][c]);
+    }
+    cprintf("/n");
+
+}
