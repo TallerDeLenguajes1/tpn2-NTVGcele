5ab7cdd6d6b125219504a2fc2f5eeb08037ca179
celeste <mesientofuerte22@gmail.com>
2020-04-04 18:18:31 -0300

    Correrir codigo y apl arit de punteros

--- tp2_1_1.cpp
+++ tp2_1_1.cpp
@@ -1,4 +1,5 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 
 #define N 4
 #define M 5
---
+++ tp2_1_2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 4
+#define M 5
+
+int main(){
+    int f,c;
+    double *mt[N][M];
+
+    for(f = 0;f<N; f++)
+    {
+        for(c = 0;c<M; c++)
+        {
+            printf("--%.2lf--", &mt[1][2]);
+        }
+        printf("\n");         
+
+    }
+    int a=0;
+    scanf("%d" , &a);
+}
